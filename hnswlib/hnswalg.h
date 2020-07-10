2fc5e81994bbea923fa4b70d3546b29cbb95d67e
Jack Wimberley <jwimberl@akamai.com>
2020-07-10 11:37:04 -0400

    Adding a method to fill a long vector of length k from a shorter buffer of size ef

--- hnswlib/hnswalg.h
+++ hnswlib/hnswalg.h
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <unordered_set>
 #include <list>
-
+#include <algorithm>
 
 namespace hnswlib {
     typedef unsigned int tableint;
@@ -332,6 +332,78 @@ namespace hnswlib {
             return top_candidates;
         }
 
+        template <bool has_deletions, bool collect_metrics=false>
+        std::vector<std::pair<dist_t, tableint>>
+        searchBaseLayerST(tableint ep_id, const void *data_point, size_t k, size_t ef) const {
+            VisitedList *vl = visited_list_pool_->getFreeVisitedList();
+            vl_type *visited_array = vl->mass;
+            vl_type visited_array_tag = vl->curV;
+
+            std::vector<std::pair<dist_t, tableint>> top_candidates;
+            std::priority_queue<std::pair<dist_t, tableint>, std::vector<std::pair<dist_t, tableint>>, CompareByFirst> top_candidates_buffer;
+            std::priority_queue<std::pair<dist_t, tableint>, std::vector<std::pair<dist_t, tableint>>, CompareByFirst> candidate_set;
+
+            if (!has_deletions || !isMarkedDeleted(ep_id)) {
+                dist_t dist = fstdistfunc_(data_point, getDataByInternalId(ep_id), dist_func_param_);
+                top_candidates_buffer.emplace(-dist, ep_id);
+                candidate_set.emplace(-dist, ep_id);
+            } else {
+                dist_t dist = std::numeric_limits<dist_t>::max();
+                candidate_set.emplace(-dist, ep_id);
+            }
+
+            visited_array[ep_id] = visited_array_tag;
+
+            while (top_candidates.size() < k && !candidate_set.empty()) {
+
+                std::pair<dist_t, tableint> current_node_pair = candidate_set.top();
+
+                candidate_set.pop();
+
+                tableint current_node_id = current_node_pair.second;
+                int *data = (int *) get_linklist0(current_node_id);
+                size_t size = getListCount((linklistsizeint*)data);
+                if(collect_metrics){
+                    metric_hops++;
+                    metric_distance_computations+=size;
+                }
+
+                for (size_t j = 1; j <= size; j++) {
+                    int candidate_id = *(data + j);
+
+                    if (!(visited_array[candidate_id] == visited_array_tag)) {
+
+                        visited_array[candidate_id] = visited_array_tag;
+
+                        char *currObj1 = (getDataByInternalId(candidate_id));
+                        dist_t dist = fstdistfunc_(data_point, currObj1, dist_func_param_);
+
+                        candidate_set.emplace(-dist, candidate_id);
+                        
+                        if (!has_deletions || !isMarkedDeleted(candidate_id)) {
+                            top_candidates_buffer.emplace(-dist, candidate_id); // closest are on top
+                            while (top_candidates_buffer.size() > ef) {
+                                top_candidates.emplace_back(top_candidates_buffer.top());
+                                top_candidates_buffer.pop();
+                                if (top_candidates.size() >= k) {
+                                    break; // breaks out of for loop
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // There might be top_candidates left in the buffer
+            while (top_candidates.size() < k && top_candidates_buffer.size() > 0) {
+                top_candidates.emplace_back(top_candidates_buffer.top());
+                top_candidates_buffer.pop();
+            }
+
+            visited_list_pool_->releaseVisitedList(vl);
+            return top_candidates;
+        }
+        
         void getNeighborsByHeuristic2(
                 std::priority_queue<std::pair<dist_t, tableint>, std::vector<std::pair<dist_t, tableint>>, CompareByFirst> &top_candidates,
         const size_t M) {
@@ -1173,6 +1245,54 @@ namespace hnswlib {
 
             return result;
         }
+
+        std::vector<std::pair<dist_t, labeltype >>
+        approxSortKnn(const void *query_data, size_t k) const {
+            std::vector<std::pair<dist_t, labeltype >> result;
+            if (cur_element_count == 0) return result;
+
+            tableint currObj = enterpoint_node_;
+            dist_t curdist = fstdistfunc_(query_data, getDataByInternalId(enterpoint_node_), dist_func_param_);
+
+            for (int level = maxlevel_; level > 0; level--) {
+                bool changed = true;
+                while (changed) {
+                    changed = false;
+                    unsigned int *data;
+
+                    data = (unsigned int *) get_linklist(currObj, level);
+                    int size = getListCount(data);
+                    metric_hops++;
+                    metric_distance_computations+=size;
+
+                    tableint *datal = (tableint *) (data + 1);
+                    for (int i = 0; i < size; i++) {
+                        tableint cand = datal[i];
+                        if (cand < 0 || cand > max_elements_)
+                            throw std::runtime_error("cand error");
+                        dist_t d = fstdistfunc_(query_data, getDataByInternalId(cand), dist_func_param_);
+
+                        if (d < curdist) {
+                            curdist = d;
+                            currObj = cand;
+                            changed = true;
+                        }
+                    }
+                }
+            }
+
+            std::vector<std::pair<dist_t, tableint>> top_candidates;
+            if (has_deletions_) {                
+                top_candidates=searchBaseLayerST<true,true>(currObj, query_data, k, ef_);
+            } else{
+                top_candidates=searchBaseLayerST<false,true>(currObj, query_data, k, ef_);
+            }
+
+            std::transform(top_candidates.begin(), top_candidates.end(), std::back_inserter(result),
+                           [this] (const std::pair<dist_t,tableint>& rez) { return std::make_pair(-rez.first,getExternalLabel(rez.second)); });
+            
+            return result;
+        };
 
         void checkIntegrity(){
             int connections_checked=0;
